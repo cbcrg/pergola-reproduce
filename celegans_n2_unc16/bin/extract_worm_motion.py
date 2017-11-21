0f14674f71d56f3154934279cce56ca4cebbed69
Jose Espinosa <kadomu@gmail.com>
2017-11-21 14:23:19 +0100

    Extracting only information used from mat files

--- celegans_n2_unc16/bin/extract_worm_motion.py
+++ celegans_n2_unc16/bin/extract_worm_motion.py
@@ -52,19 +52,19 @@ file_name = file_name.replace (" ", "_")
 f = h5py.File(input_file)
 
 ### INFO
-sex_r = f['info']['experiment']['worm']['sex']
-sex = str(''.join(unichr(c) for c in sex_r))
-
-habituation_r = f['info']['experiment']['worm']['habituation']
-habituation = str(''.join(unichr(c) for c in habituation_r))
-
-# annotations (empty)
-annotations_r = f['info']['experiment']['environment']['annotations']
-annotations = str(''.join(c.astype(str) for c in annotations_r))
-
-# info/experiment/worm/genotype
-genotype_r = f['info']['experiment']['worm']['genotype'] #type u2
-genotype = str(''.join(unichr(c) for c in genotype_r))
+# sex_r = f['info']['experiment']['worm']['sex']
+# sex = str(''.join(unichr(c) for c in sex_r))
+#
+# habituation_r = f['info']['experiment']['worm']['habituation']
+# habituation = str(''.join(unichr(c) for c in habituation_r))
+#
+# # annotations (empty)
+# annotations_r = f['info']['experiment']['environment']['annotations']
+# annotations = str(''.join(c.astype(str) for c in annotations_r))
+#
+# # info/experiment/worm/genotype
+# genotype_r = f['info']['experiment']['worm']['genotype'] #type u2
+# genotype = str(''.join(unichr(c) for c in genotype_r))
 
 # /info/experiment/worm/strain
 strain_r = f['info']['experiment']['worm']['strain']
@@ -72,24 +72,24 @@ strain = str(''.join(unichr(c) for c in strain_r))
 
 # age worm
 # /info/experiment/worm/age
-age_r = f['info']['experiment']['worm']['age'] #type u2
-age = str(''.join(unichr(c) for c in age_r))
-
-# /info/experiment/environment/food
-food_r = f['info']['experiment']['environment']['food'] #type u2
-food = str(''.join(unichr(c) for c in food_r))
-
-# /info/experiment/environment/timestamp
-timestamp_r = f['info']['experiment']['environment']['timestamp'] #type u2
-timestamp = str(''.join(unichr(c) for c in timestamp_r))
-
-# HH:MM:SS.mmmmmm
-my_date_object = strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
-unix_time = timegm(my_date_object) # utc based # correct!!!
-
-# /info/video/length/time
-time_recorded_r = f['info']['video']['length']['time']
-time_recorded = time_recorded_r[0][0]
+# age_r = f['info']['experiment']['worm']['age'] #type u2
+# age = str(''.join(unichr(c) for c in age_r))
+#
+# # /info/experiment/environment/food
+# food_r = f['info']['experiment']['environment']['food'] #type u2
+# food = str(''.join(unichr(c) for c in food_r))
+#
+# # /info/experiment/environment/timestamp
+# timestamp_r = f['info']['experiment']['environment']['timestamp'] #type u2
+# timestamp = str(''.join(unichr(c) for c in timestamp_r))
+#
+# # HH:MM:SS.mmmmmm
+# my_date_object = strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+# unix_time = timegm(my_date_object) # utc based # correct!!!
+#
+# # /info/video/length/time
+# time_recorded_r = f['info']['video']['length']['time']
+# time_recorded = time_recorded_r[0][0]
 
 # /info/video/length/frames
 frames_r = f['info']['video']['length']['frames']
@@ -121,16 +121,16 @@ for motion_k in sorted(motion_keys):
 
     fh = open(file_name + "." + motion_k + ".csv",'wb')
 
-    fh.write("#genotype;%s\n" % genotype)
+    # fh.write("#genotype;%s\n" % genotype)
     fh.write("#strain;%s\n" % strain)
-    fh.write("#age;%s\n" % age)
-    fh.write("#habituation;%s\n" % habituation)
-    fh.write("#food;%s\n" % food)
-    fh.write("#unix_time;%s\n" % unix_time)
-    fh.write("#time_recorded;%s\n" % time_recorded)
+    # fh.write("#age;%s\n" % age)
+    # fh.write("#habituation;%s\n" % habituation)
+    # fh.write("#food;%s\n" % food)
+    # fh.write("#unix_time;%s\n" % unix_time)
+    # fh.write("#time_recorded;%s\n" % time_recorded)
     fh.write("#frames;%s\n" % frames)
     fh.write("#fps;%s\n" % fps)
-    fh.write("#annotations;%s\n" % annotations)
+    # fh.write("#annotations;%s\n" % annotations)
 
     writer_out = writer(fh, dialect = 'excel-tab')
     
--- celegans_n2_unc16/bin/extract_worm_speed.py
+++ celegans_n2_unc16/bin/extract_worm_speed.py
@@ -54,19 +54,19 @@ file_name = file_name.replace (" ", "_")
 f = h5py.File(input_file)
 
 ### INFO
-sex_r = f['info']['experiment']['worm']['sex']
-sex = str(''.join(unichr(c) for c in sex_r))
+# sex_r = f['info']['experiment']['worm']['sex']
+# sex = str(''.join(unichr(c) for c in sex_r))
 
-habituation_r = f['info']['experiment']['worm']['habituation']
-habituation = str(''.join(unichr(c) for c in habituation_r))
+# habituation_r = f['info']['experiment']['worm']['habituation']
+# habituation = str(''.join(unichr(c) for c in habituation_r))
 
 # annotations (empty)
-annotations_r = f['info']['experiment']['environment']['annotations']
-annotations = str(''.join(c.astype(str) for c in annotations_r))
+# annotations_r = f['info']['experiment']['environment']['annotations']
+# annotations = str(''.join(c.astype(str) for c in annotations_r))
 
 # info/experiment/worm/genotype
-genotype_r = f['info']['experiment']['worm']['genotype'] #type u2
-genotype = str(''.join(unichr(c) for c in genotype_r))
+# genotype_r = f['info']['experiment']['worm']['genotype'] #type u2
+# genotype = str(''.join(unichr(c) for c in genotype_r))
 
 # /info/experiment/worm/strain
 strain_r = f['info']['experiment']['worm']['strain']
@@ -74,24 +74,24 @@ strain = str(''.join(unichr(c) for c in strain_r))
 
 # age worm
 # /info/experiment/worm/age
-age_r = f['info']['experiment']['worm']['age'] #type u2
-age = str(''.join(unichr(c) for c in age_r))
+# age_r = f['info']['experiment']['worm']['age'] #type u2
+# age = str(''.join(unichr(c) for c in age_r))
 
 # /info/experiment/environment/food
-food_r = f['info']['experiment']['environment']['food'] #type u2
-food = str(''.join(unichr(c) for c in food_r))
+# food_r = f['info']['experiment']['environment']['food'] #type u2
+# food = str(''.join(unichr(c) for c in food_r))
 
 # /info/experiment/environment/timestamp
-timestamp_r = f['info']['experiment']['environment']['timestamp'] #type u2
-timestamp = str(''.join(unichr(c) for c in timestamp_r))
+# timestamp_r = f['info']['experiment']['environment']['timestamp'] #type u2
+# timestamp = str(''.join(unichr(c) for c in timestamp_r))
 
 # HH:MM:SS.mmmmmm
-my_date_object = strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
-unix_time = timegm(my_date_object) # utc based # correct!!!
+# my_date_object = strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+# unix_time = timegm(my_date_object) # utc based # correct!!!
 
 # /info/video/length/time
-time_recorded_r = f['info']['video']['length']['time']
-time_recorded = time_recorded_r[0][0]
+# time_recorded_r = f['info']['video']['length']['time']
+# time_recorded = time_recorded_r[0][0]
 
 # /info/video/length/frames
 frames_r = f['info']['video']['length']['frames']
@@ -105,16 +105,16 @@ velocity_keys = ['head', 'headTip', 'midbody', 'tail', 'tailTip']
 
 fh = open(file_name + "_speed.csv",'wb')
 
-fh.write("#genotype;%s\n" % genotype)
+# fh.write("#genotype;%s\n" % genotype)
 fh.write("#strain;%s\n" % strain)
-fh.write("#age;%s\n" % age)
-fh.write("#habituation;%s\n" % habituation)
-fh.write("#food;%s\n" % food)
-fh.write("#unix_time;%s\n" % unix_time)
-fh.write("#time_recorded;%s\n" % time_recorded)
+# fh.write("#age;%s\n" % age)
+# fh.write("#habituation;%s\n" % habituation)
+# fh.write("#food;%s\n" % food)
+# fh.write("#unix_time;%s\n" % unix_time)
+# fh.write("#time_recorded;%s\n" % time_recorded)
 fh.write("#frames;%s\n" % frames)
 fh.write("#fps;%s\n" % fps)
-fh.write("#annotations;%s\n" % annotations)
+# fh.write("#annotations;%s\n" % annotations)
 
 writer_out = writer(fh, dialect = 'excel-tab')
 
@@ -140,7 +140,6 @@ except KeyError:
  
 # range already substract one to frames 
 for frame in range(0, int(frames)):
-# for frame in range(0, 100):    #del #debug
     list_v = list()
     list_v.extend ([frame, frame+1])
     
